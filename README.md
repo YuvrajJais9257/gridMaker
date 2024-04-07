7f0d9903b071d175de5c233bccfd1e03aa4cd724
Yuvraj Jaiswal <yuvraj.jaiswal@erasmith.com>
2024-04-08 00:25:08 +0530

    improved version

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gridify
+
+Gridify is a simple tool for creating customizable grids. With just a few lines of code, you can generate grids of various sizes for your projects.
+
+## Features
+
+- Generate grids of any size.
+- Customizable grid styling.
+- Easy to integrate into any project.
+
--- script.js
+++ script.js
@@ -19,16 +19,21 @@
 //blueBtn.addEventListener('click', ()=>{
 //    blue.classList.toggle('blue');
 //})
-const gridContainer=document.getElementById( 'grid-container' );
-function getColor(row, col) {
+//const gridContainer=document.getElementById( 'grid-container' );
+
+ function generateGrid(){
+  const rows=parseInt(document.getElementById( "rows" ).value ) ;
+  const columns=parseInt(document.getElementById("columns").value);
+  const gridContainer=document.getElementById('grid-container');
+  function getColor(row, col) {
     return (row + col) % 2 === 0 ? 'white' : 'black';
   }
   
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < rows; i++) {
     const row = document.createElement('div');
     row.classList.add('row'); 
   
-    for (let j = 0; j < 8; j++) {
+    for (let j = 0; j < columns; j++) {
       const grid = document.createElement('div');
       grid.classList.add('grid');
       grid.style.backgroundColor = getColor(i, j);
@@ -37,3 +42,4 @@ function getColor(row, col) {
   
     gridContainer.appendChild(row);
   }
+}
